96d6df7e160f1e0d4072e304042cee2a271e89ff
HongYongGi <hyg4438@gachon.ac.kr>
2024-06-26 11:58:53 +0900

    add code

---
+++ utils/__init__.py
@@ -0,0 +1,8 @@
+__all__=[
+    "load_data",    
+    "convert_mha_nii",
+    "convert_nrrd_nii",
+    "convert_dicom_nii",
+    "makedir"
+]
+from .convert import load_data, convert_mha_nii, convert_nrrd_nii, convert_dicom_nii, makedir
