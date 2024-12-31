635f87c429f9d5fb6b37edb68376fdb1c78c1730
YONGGI HONG <39263586+HongYongGi@users.noreply.github.com>
2024-12-31 15:43:45 +0900

    Refactor prediction notebook and remove unused utility files. Added necessary imports and updated kernel specifications in `prediction.ipynb`. Deleted `__init__.py` and `convert.py` from the `utils` directory as they were no longer needed.

--- utils/__init__.py
+++
@@ -1,8 +0,0 @@
-__all__=[
-    "load_data",    
-    "convert_mha_nii",
-    "convert_nrrd_nii",
-    "convert_dicom_nii",
-    "makedir"
-]
-from .convert import load_data, convert_mha_nii, convert_nrrd_nii, convert_dicom_nii, makedir
